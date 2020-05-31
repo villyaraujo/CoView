2ca5558aad252af4a52f99f8c512e0943c3e18bf
Gabriel Babler <gabrielbabler@yahoo.com.br>
2020-05-31 19:07:48 -0300

    add new tab teams

---
+++ src/pages/team/team.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-team',
+  templateUrl: 'team.html'
+})
+export class TeamPage {
+
+    constructor(public navCtrl: NavController) {
+    }
+
+}
